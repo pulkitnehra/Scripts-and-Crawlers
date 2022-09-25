e0000648c8473a4becee1075677f2d052da1a48c
Pulkit Nehra <65600727+pulkitnehra@users.noreply.github.com>
2022-09-25 16:39:15 +0530

    Update ubuntu-install-programs.sh

--- ubuntu-install-programs.sh
+++ ubuntu-install-programs.sh
@@ -15,9 +15,14 @@ sudo apt install -yy openjdk-11-jdk
 # # install eclipse
 sudo snap install eclipse --classic
 
-# # install mysql server
+# # install sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update && sudo apt-get install sublime-text -yy
 
-#install the latest version of git
+#
+
+# install the latest version of git
 sudo add-apt-repository -qq ppa:git-core/ppa
 sudo apt-get install git -yy
 
